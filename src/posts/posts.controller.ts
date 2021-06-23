3eb1d188f98d42a91e12b201e7cdded11b754b2a
Arnav Gupta <arnav.gupta@scaler.com>
2021-06-23 23:43:06 +0530

    posts controller routes all done
    Signed-off-by: Arnav Gupta <arnav.gupta@scaler.com>

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Delete, Param, Post, Put } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get('/')
+  getAllPosts(): string {
+    // TODO
+    return 'get all posts';
+  }
+
+  @Get('/:postid')
+  getPostDetails(@Param() param): string {
+    // TODO
+    return `details of postid = ${param.postid}`;
+  }
+
+  @Post('/')
+  createNewPost(): string {
+    // TODO
+    return `new post was created`;
+  }
+
+  @Delete('/:postid')
+  deletePost(@Param() param): string {
+    // TODO
+    return `delete postid = ${param.postid}`;
+  }
+
+  @Put('/:postid/like')
+  likePost(@Param() param): string {
+    return `liked post ${param.postid}`;
+  }
+
+  @Delete('/:postid/like')
+  unlikePost(@Param() param): string {
+    return `unliked post ${param.postid}`;
+  }
+}
