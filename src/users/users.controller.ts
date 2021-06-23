b4955f6cb5dcf8f42d4fc9f9b05fdb6b07cd3ee7
Arnav Gupta <arnav.gupta@scaler.com>
2021-06-23 23:23:23 +0530

    all endpoints for user controller are done
    Signed-off-by: Arnav Gupta <arnav.gupta@scaler.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Delete } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/@:username')
+  getUserByUsername(@Param() param): string {
+    // TODO
+    return `details of username = ${param.username}`;
+  }
+
+  @Get('/:userid')
+  getUserByUserid(@Param() param): string {
+    // TODO
+    return `details of user id = ${param.userid}`;
+  }
+
+  @Post('/')
+  createNewUser(): string {
+    return 'new user created';
+  }
+
+  @Patch('/:userid')
+  updateUserDetails(@Param() param): string {
+    return `details of user (id = ${param.userid}) updated`;
+  }
+
+  @Put('/:userid/follow')
+  followUser(): string {
+    return 'you followed user';
+  }
+
+  @Delete('/:userid/follow')
+  unfollowUser(): string {
+    return 'you unfollowed user';
+  }
+
+  @Get('/:userid/followers')
+  getFollowersOfUser(): string {
+    return 'get followers of user';
+  }
+
+  @Put('/:userid/followees')
+  getFolloweesOfUser(): string {
+    return `get followees of given user`;
+  }
+}
