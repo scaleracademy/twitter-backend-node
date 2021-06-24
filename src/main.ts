9dc397dcdfd95c9952ebf947cfb3ccca1b8141be
alpha037 <subhronil.dutta@gmail.com>
2021-06-24 19:27:52 +0530

    FEAT: Add Response Compression

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Use Response Compression
+  app.use(compression());
+
   await app.listen(3000);
 }
 bootstrap();
