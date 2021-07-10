e75f6d2c022ab100092e3a6ab48a2ab020b4bc0b
Arnav Gupta <arnav.gupta@scaler.com>
2021-07-11 03:26:17 +0530

    add auth guards to other needed user endpoints
    Signed-off-by: Arnav Gupta <arnav.gupta@scaler.com>

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -85,21 +85,29 @@ export class UsersController {
     return user;
   }
 
+  @ApiBearerAuth()
+  @UseGuards(RequiredAuthGuard)
   @Put('/:userid/follow')
   followUser(): string {
     return 'you followed user';
   }
 
+  @ApiBearerAuth()
+  @UseGuards(RequiredAuthGuard)
   @Delete('/:userid/follow')
   unfollowUser(): string {
     return 'you unfollowed user';
   }
 
+  @ApiBearerAuth()
+  @UseGuards(RequiredAuthGuard)
   @Get('/:userid/followers')
   getFollowersOfUser(): string {
     return 'get followers of user';
   }
 
+  @ApiBearerAuth()
+  @UseGuards(RequiredAuthGuard)
   @Put('/:userid/followees')
   getFolloweesOfUser(): string {
     return `get followees of given user`;
