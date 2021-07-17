e5176fea48d582aaaa47015ad3b9da042070c5b6
Arnav Gupta <arnav.gupta@scaler.com>
2021-07-18 04:13:10 +0530

    INFRA: update packages + fix e2e tests for fastify
    Signed-off-by: Arnav Gupta <arnav.gupta@scaler.com>

--- test/users/users.e2e-spec.ts
+++ test/users/users.e2e-spec.ts
@@ -3,37 +3,41 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { ApiModule } from 'src/api.module';
 import { TestDbModule } from 'src/commons/db.module';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
 
 describe('UsersController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [ApiModule, TestDbModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
     await app.init();
   });
 
   // test user creation
   it('(POST) /users/', () => {
-    return request(app.getHttpServer())
-      .post('/users')
-      .send({
-        username: 'arnav',
-        password: 'arnav123',
-        name: 'Arnav Gupta',
-        bio: 'This is a nice guy!',
+    return app
+      .inject({
+        method: 'POST',
+        url: '/users',
+        payload: {
+          username: 'arnav',
+          password: 'arnav123',
+          name: 'Arnav Gupta',
+          bio: 'This is a nice guy!',
+        },
       })
-      .expect(201);
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/hello')
-      .expect(200)
-      .expect('Hello World!');
+      .then((response) => {
+        expect(response.statusCode).toBe(201);
+      });
   });
 
   afterAll(async () => {
